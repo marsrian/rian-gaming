13a048b030a1ef06ae5768defed827c975e505f5
marsrian <marsrian40@gmail.com>
2024-06-16 01:51:06 +0600

    blog-6

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 import HeroSection from "@/components/HeroSection";
-import RecentBlogs from "@/components/RecentBlogs";
-import RecentGames from "@/components/RecentGames";
+// import RecentBlogs from "@/components/RecentBlogs";
+// import RecentGames from "@/components/RecentGames";
 
 const HomePage = () => {
   return (
     <div className="mt-8 mb-6">
       <HeroSection />
-      <div className="mt-20">
+      {/* <div className="mt-20">
         <RecentGames />
         <div className="mt-20">
           <RecentBlogs />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
