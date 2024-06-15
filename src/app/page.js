c428b24658c3f428ba1025a2e6a6a6b9f2746663
marsrian <marsrian40@gmail.com>
2024-06-16 02:36:13 +0600

    blog-9

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,22 @@
 import HeroSection from "@/components/HeroSection";
+import RecentBlogs from "@/components/RecentBlogs";
 import RecentGames from "@/components/RecentGames";
+import { Suspense } from "react";
 
 const HomePage = () => {
   return (
     <div className="mt-8 mb-6">
       <HeroSection />
       <div className="mt-20">
-        <RecentGames />
+        <Suspense fallback={<p>Loading...</p>}>
+          <RecentGames />
+        </Suspense>
       </div>
-      {/* <div className="mt-20">
-        <RecentBlogs />
-      </div> */}
+      <div className="mt-20">
+        <Suspense fallback={<p>Loading...</p>}>
+          <RecentBlogs />
+        </Suspense>
+      </div>
     </div>
   );
 };
