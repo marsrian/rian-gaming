5040677bdf324989dbca21efc6d1c407c155eed3
marsrian <marsrian40@gmail.com>
2024-06-18 03:17:51 +0600

    blog-143

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,12 @@ const HomePage = () => {
     <div className="mt-8 mb-6">
       <HeroSection />
       <div className="mt-20">
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<p>Loading Game Info...</p>}>
           <RecentGames />
         </Suspense>
       </div>
       <div className="mt-20">
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<p>Loading Blog Info...</p>}>
           <RecentBlogs />
         </Suspense>
       </div>
